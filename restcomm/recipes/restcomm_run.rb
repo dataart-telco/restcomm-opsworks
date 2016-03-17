adf31695d5e056ce41085aecaabf94f494c1a0f5
root <root@restcomm1.localdomain>
2016-03-17 16:02:44 +0000

    add loadbalancers to restcom

--- restcomm/recipes/restcomm_run.rb
+++ restcomm/recipes/restcomm_run.rb
@@ -3,7 +3,10 @@
 # Recipe:: run
 #
 
-data = node['restcomm'] == nil ? node['restcomm'] : node
+lbLayer = search("aws_opsworks_layer", "name:restcomm-loadbalancer").first
+loadBalancers = search("aws_opsworks_instance", "layer_ids:#{lbLayer['layer_id']}").each.map { |i| "#{i['private_ip']}:5065"}.join(',')
+
+data = node['restcomm'] != nil ? node['restcomm'] : node
 
 docker_container 'restcomm' do
   action :delete
@@ -13,6 +16,6 @@ docker_container 'restcomm' do
   network_mode 'host'
   repo 'hamsterksu/restcomm'
   tag '7.6.0.831'
-  env ["INIT_PASSWORD=#{data['INIT_PASSWORD']}", "VOICERSS_KYE=#{data['VOICERSS_KYE']}", "MEDIASERVER_LOWEST_PORT=#{data['MEDIASERVER_LOWEST_PORT']}", "MEDIASERVER_HIGHEST_PORT=#{data['MEDIASERVER_HIGHEST_PORT']}"]
+  env ["INIT_PASSWORD=#{data['INIT_PASSWORD']}", "VOICERSS_KYE=#{data['VOICERSS_KYE']}", "MEDIASERVER_LOWEST_PORT=#{data['MEDIASERVER_LOWEST_PORT']}", "MEDIASERVER_HIGHEST_PORT=#{data['MEDIASERVER_HIGHEST_PORT']}", "LOAD_BALANCERS=#{loadBalancers}"]
   action :run
 end
